1d7ad2cf6023e73118647503f5606e5eb80099bd
cemcebeci <cemcebeci99@gmail.com>
2020-04-07 08:05:19 +0200

    Main class created and functionality shown.

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Main
+{
+    public static void main( String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>( Arrays.asList( 100, 500, 900));
+        ArrayList<Integer> b = new ArrayList<Integer>( Arrays.asList( 800, 600, 300));
+        ArrayList<Integer> c = new ArrayList<Integer>( Arrays.asList( 400, 700, 200));
+
+        ArrayOrganizer org = new ArrayOrganizer();
+
+        System.out.println( org.concatAndSort(a, b, c));
+    }
+}
